7757a11f4dff89d2ead1b84db5f3fa7fd2e1f0a8
rezaul28 <rezaul.csedu@gmail.com>
2022-04-24 00:55:32 +0600

    .

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 root = "/home/vuzinga/Documents/Python/bd-vehicle-classifier/"
 video =cv2.VideoCapture("./resource/cars.mp4")
 car = "./resource/car.xml"
+bus="./resource/bus.xml"
 img_folder=root+"photos"
 try:
     os.remove(root+"new.mp4")
@@ -16,33 +17,37 @@ except:
     pass
 os.mkdir(root+"photos")
 car_tracker = cv2.CascadeClassifier(car)
+bus_tracker = cv2.CascadeClassifier(bus)
 i=0
 while True:
     (read,fram) = video.read()
     if(read):
-        fram = cv2.resize(fram, (700,450), interpolation = cv2.INTER_AREA)
+        fram = cv2.resize(fram, (300,300), interpolation = cv2.INTER_AREA)
         gray_image = cv2.cvtColor(fram,cv2.COLOR_BGR2GRAY)
     else:
         break
-    cars = car_tracker.detectMultiScale(fram)
+    cars = car_tracker.detectMultiScale(gray_image)
+    buss = bus_tracker.detectMultiScale(gray_image)
     for (x,y,w,h) in cars:
         cv2.rectangle(fram,(x,y),(x+w,y+h),(255,0,255))
-    cv2.imwrite(os.path.join( img_folder, str(i)+'.jpg'), fram) 
-    i+=1
-print("Finished sampling")
-images = [img for img in os.listdir(img_folder) if img.endswith(".jpg")]
-print()
-def myFunc(e):
-  return int(e[:-4])
-images.sort(key=myFunc)
-frame = cv2.imread(os.path.join(img_folder, images[0]))
-height, width, layers = frame.shape
-video = cv2.VideoWriter(root+"new.mp4",int(video.get(cv2.CAP_PROP_FOURCC)) , video.get(5), (width,height))
+    for(x,y,w,h) in buss:
+        cv2.rectangle(fram,(x,y),(x+w,y+h),(0,0,0))
+    cv2.imshow("Processed image",gray_image)
+    cv2.waitKey(1)
+# print("Finished sampling")
+# images = [img for img in os.listdir(img_folder) if img.endswith(".jpg")]
+# print()
+# def myFunc(e):
+#   return int(e[:-4])
+# images.sort(key=myFunc)
+# frame = cv2.imread(os.path.join(img_folder, images[0]))
+# height, width, layers = frame.shape
+# video = cv2.VideoWriter(root+"new.mp4",int(video.get(cv2.CAP_PROP_FOURCC)) , video.get(5), (width,height))
 
-for image in images:
-    video.write(cv2.imread(os.path.join(img_folder, image)))
-try:
-    shutil.rmtree(root+"photos",ignore_errors=False, onerror=None)
-except:
-    pass
-sys.stdout.flush()
+# for image in images:
+#     video.write(cv2.imread(os.path.join(img_folder, image)))
+# try:
+#     shutil.rmtree(root+"photos",ignore_errors=False, onerror=None)
+# except:
+#     pass
+# sys.stdout.flush()
